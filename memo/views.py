a02c05ac847eff4001a211526a87633f7574f832
pascal <pascal.allau@formation-occ.com>
2019-07-23 19:18:39 +0200

    création du fichier base.html

--- memo/views.py
+++ memo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'memo/welcome.html')
+    return render(request, 'memo/welcome.html', {'current_date_time':datetime.now})
